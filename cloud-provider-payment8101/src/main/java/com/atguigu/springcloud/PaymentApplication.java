85b1eb79c83d08219ccfca537284d1849cbb13b6
Hushunkang <2752962035@qq.com>
2020-04-28 22:40:00 +0800

    把支付微服务注册到eureka服务端

--- cloud-provider-payment8101/src/main/java/com/atguigu/springcloud/PaymentApplication.java
+++ cloud-provider-payment8101/src/main/java/com/atguigu/springcloud/PaymentApplication.java
@@ -2,6 +2,7 @@ package com.atguigu.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * @author hskBeginner Email：2752962035@qq.com
@@ -10,6 +11,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @create 2020年04月28日
  */
 @SpringBootApplication
+@EnableEurekaClient//表示本微服务（我）就是要被注册到注册中心的微服务（微服务的提供者），即eureka服务端
 public class PaymentApplication {
 
     public static void main(String[] args) {
