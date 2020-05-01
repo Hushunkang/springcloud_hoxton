003f34cc1bd7249cb12cfa73f3769462118c8fec
Hushunkang <2752962035@qq.com>
2020-05-01 12:34:30 +0800

    订单微服务注册到zookeeper，为临时节点，一段时间没接受到微服务的心跳，不管是真死还是假死，剔除掉

---
+++ cloud-consumer-order8105/src/main/java/com/atguigu/springcloud/config/ApplicationContextConfig.java
@@ -0,0 +1,21 @@
+package com.atguigu.springcloud.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
+
+/**
+ * @author hskBeginner Email：2752962035@qq.com
+ * @version 1.0
+ * @description
+ * @create 2020年04月28日
+ */
+@Configuration
+public class ApplicationContextConfig {
+
+    @Bean
+    public RestTemplate restTemplate(){
+        return new RestTemplate();
+    }
+
+}
---
+++ cloud-consumer-order8105/src/main/java/com/atguigu/springcloud/controller/OrderController.java
@@ -0,0 +1,32 @@
+package com.atguigu.springcloud.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+/**
+ * @author hskBeginner Email：2752962035@qq.com
+ * @version 1.0
+ * @description
+ * @create 2020年05月01日
+ */
+@RestController
+@RequestMapping("/order")
+@Slf4j
+public class OrderController {
+
+    public static final String PAYMENT_URL = "http://cloud-provider-payment";
+
+    @Autowired
+    private RestTemplate restTemplate;
+
+    @GetMapping("paymentInfo")
+    public String paymentInfo(){
+        String result = restTemplate.getForObject(PAYMENT_URL + "/payment/zookeeper",String.class);
+        return result;
+    }
+
+}
