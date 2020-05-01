3b2e6f7fb0d0d15ba1c0c5366c0d6dc27f861008
Hushunkang <2752962035@qq.com>
2020-05-01 15:50:41 +0800

    微服务的provider 注册到注册中心（consul）

---
+++ cloud-provider-payment8106/src/main/java/com/atguigu/springcloud/PaymentApplication8106.java
@@ -0,0 +1,22 @@
+package com.atguigu.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @author hskBeginner Email：2752962035@qq.com
+ * @version 1.0
+ * @description
+ * @create 2020年05月01日
+ */
+@SpringBootApplication
+@EnableDiscoveryClient//服务（微服务的提供者）发现，对于注册到注册中心中的微服务的提供者，可以通过服务发现来获得它的信息
+//EnableDiscoveryClient注解用于向使用nacos、consul、zookeeper作为注册中心的时候，提供服务发现功能
+public class PaymentApplication8106 {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PaymentApplication8106.class,args);
+    }
+
+}
---
+++ cloud-provider-payment8106/src/main/java/com/atguigu/springcloud/controller/PaymentController.java
@@ -0,0 +1,30 @@
+package com.atguigu.springcloud.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.UUID;
+
+/**
+ * @author hskBeginner Email：2752962035@qq.com
+ * @version 1.0
+ * @description
+ * @create 2020年04月30日
+ */
+@RestController
+@RequestMapping("/payment")
+@Slf4j
+public class PaymentController {
+
+    @Value("${server.port}")
+    private String serverPort;
+
+    @GetMapping(value = "consul")
+    public String paymentConsul() {
+        return "spring boot with consul:" + serverPort + ":" +UUID.randomUUID().toString();
+    }
+
+}
